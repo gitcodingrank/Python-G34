27b1281da0427ca2eb1d50968eff7ee58e15717e
gitcodingrank <jiraja2915@gmail.com>
2024-08-29 19:18:38 +0530

    Done

---
+++ Unit 1/Sprint 2/Day 2/fStringExample.py
@@ -0,0 +1,45 @@
+#F-String: using this concept, you can directly use/embed variable and expression into the string.
+
+#life with f string
+
+# name = "Alice"
+# age = 45
+# #Hi, I'm Alice and I'm 45 years old.
+# print("Hi, I'm", name, "and I'm", age, "years old.")
+
+# #using F-String
+# print(f"Hi, I'm {name} and I'm {age} years old.")
+
+#Good Example 1: Create Dynamic Email
+
+# email = "
+# Dear Rakesh
+
+# I'm from Masai, where i will teach you about Python Programming Languange.
+
+# Best Regards
+# Anand Singh Yadav
+# "
+
+#Note: if you're creating multi-line string using double quatation mark then it will give you error then what is the solution for multi-line string: triple quatation marks("""  """)
+
+#triple quatation marks("""  """):
+# your python doesn't support multi-line comment, but you can use below method to keep multi line comment but this is not recommended.
+#You can use it for creating multi-line string.
+
+studentName = input("Enter Student Name: ")
+teacherOrg = input("Enter Teacher Organization: ")
+programmingLanguange = input("Ente Programming Language: ")
+teacherName = input("Enter Teacher Name: ")
+
+email = f"""
+Dear {studentName}
+
+I'm from {teacherOrg}, where i will teach you about {programmingLanguange} Programming Languange.
+
+Best Regards
+{teacherName}
+"""
+
+print(email)
+
---
+++ Unit 1/Sprint 2/Day 2/moreAboutInputOutputStatementExample.py
@@ -0,0 +1,55 @@
+#Good Example 1: take, name, age, and city from user and print on the console.
+
+#taking input from user
+# name = input("Enter Name: ")
+# age = input("Enter Age: ")
+# city = input("Enter City: ")
+
+#printing the value
+# print("User Name:", name)
+# print("User Age:", age)
+# print("User City:", city)
+
+#The data you're getting from input() function, do you know the type of data?
+
+# print("Type of Name Value:", type(name))
+# print("Type of City Value:", type(city))
+# print("Type of Age Value:", type(age))
+
+#I want to convert string to integer and integer to float.
+#Type Conversion: you convert type of data to another type.
+
+#Example 1: 
+# age = 45
+# ageToFloat = float(age)
+# ageToString = str(age)
+# print(age, type(age)) #<class, int>
+# print(ageToFloat, type(ageToFloat)) # <class, float>
+# print(ageToString, type(ageToString)) # <class, str>
+
+# #Example 2:
+
+# value = "45"
+# intValue = int(value)
+# floatValue = float(value)
+# print(value, type(value)) #<class, str>
+# print(intValue, type(intValue)) #<class, int>
+# print(floatValue, type(floatValue)) #<class, float>
+
+#Good Example 2: take, name, age, and city from user and print on the console.
+
+#taking input from user
+# name = input("Enter Name: ")
+# age = int(input("Enter Age: "))
+# city = input("Enter City: ")
+
+#printing the value
+# print("User Name:", name)
+# print("User Age:", age, type(age))
+# print("User City:", city)
+
+#Create 
+# Dynamic Report Card:
+
+
+
